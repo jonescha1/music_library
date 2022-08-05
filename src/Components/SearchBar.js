4f6000d02a53f73f87755e64ad99035c4d51669f
Chase Jones <chaseejones@icloud.com>
2022-08-04 21:28:09 -0400

    added a search context

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { SearchContext } from "../Context/SearchContext";
 
-function SearchBar(props) {
+function SearchBar() {
+  const { term, handleSearch } = useContext(SearchContext);
+
   return (
     <form>
-      <input
-        type="text"
-        placeholder="Search Here"
-        onChange={(e) => props.handleSearch(e, e.target.value)}
-      />
-      <input type="submit" />
+      <input ref={term} type="text" placeholder="Search Here" />
+      <button onClick={(e) => handleSearch(e, term.current.value)}>
+        Submit
+      </button>
     </form>
   );
 }
